b8f6cb8f1ea7988ee188d3813212cad0902af4f2
Casper Bruun <cbm@berlingskemedia.dk>
2017-06-09 11:56:07 +0200

    Enable ssh server

--- prequisites.sh
+++ prequisites.sh
@@ -4,4 +4,6 @@ sudo apt-get -y upgrade
 sudo apt-get install -y raspberrypi-ui-mods chromium-browser git
 # Ensure screen is not going blank.
 sudo touch /boot/alwayson
+# Enable SSH server
+sudo touch /boot/ssh
 sudo reboot
