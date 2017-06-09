3534c7cd67acb4f5cb7d75f82e9a6349098b4d43
Casper Bruun <cbm@berlingskemedia.dk>
2017-06-09 14:03:20 +0200

    /boot/alwayson file deprecated

--- prequisites.sh
+++ prequisites.sh
@@ -2,8 +2,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y raspberrypi-ui-mods chromium-browser git
-# Ensure screen is not going blank.
-sudo touch /boot/alwayson
 # Enable SSH server
 sudo touch /boot/ssh
 # Disable screensaver
