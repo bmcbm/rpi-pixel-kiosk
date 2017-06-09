cc1e4cab5cf2f2c311bd70be3d09c1d7a2e3ab27
Casper Bruun <cbm@berlingskemedia.dk>
2017-06-09 11:49:53 +0200

    touch /boot/alwayson file

--- prequisites.sh
+++ prequisites.sh
@@ -2,4 +2,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y raspberrypi-ui-mods chromium-browser git
+# Ensure screen is not going blank.
+sudo touch /boot/alwayson
 sudo reboot
