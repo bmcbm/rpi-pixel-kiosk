8ab85d4bb8f37db3188156d360ad512be7b2fbbb
Casper Bruun <cbm@berlingskemedia.dk>
2017-06-09 12:56:01 +0200

    Diable Chromium errer dialogs

--- home/pi/start-kiosk.sh
+++ home/pi/start-kiosk.sh
@@ -8,4 +8,4 @@ fi
 # clean up if like me you just yank the power or ssh in and poweroff
 rm -rf ~/.config/chromium/Singleton*
 # Launch chromium in kiosk mode, incognito with a reputable, well-designed url
-chromium-browser --kiosk --incognito https://www.codesign2.co.uk
+chromium-browser --noerrdialogs --kiosk --incognito https://www.codesign2.co.uk
