143f54e41b0bc3efc7737f9c9aab728f36c88026
Casper Bruun <cbm@berlingskemedia.dk>
2017-06-09 11:42:43 +0200

    move installation of chromium and pixel to prerequisites.sh script

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y raspberrypi-ui-mods chromium-browser
 cp -raf home /
 systemctl --user enable kiosk
 systemctl --user start kiosk
