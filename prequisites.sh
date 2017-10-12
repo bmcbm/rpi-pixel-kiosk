0fbe00d45aba360753a90fd4c01a2ca0ac91d2c0
Casper Bruun <cbm@x32.dk>
2017-10-12 10:57:26 +0200

    Disable overscan to use full screen real estate

--- prequisites.sh
+++ prequisites.sh
@@ -6,4 +6,6 @@ sudo apt-get install -y raspberrypi-ui-mods chromium-browser git
 sudo touch /boot/ssh
 # Disable screensaver
 sudo cp -af home/pi/.config/lxsession/LXDE-pi/autostart /etc/xdg/lxsession/LXDE-pi/autostart
+# Disable overscan to use full screen real estate
+echo -e "\n# rpi-pixel-koisk - disable overscan\ndisable_overscan=1" >> /boot/config.txt
 sudo reboot
